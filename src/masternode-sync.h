3489aad7c05dbaa1ce47d73e0a9132f18bed4464
citypayorg <citypay.org@gmail.com>
2020-08-12 21:49:49 -0700

    masternode

--- src/masternode-sync.h
+++ src/masternode-sync.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2019 The Ctp Core developers
+// Copyright (c) 2014-2017 The Dash Core developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef MASTERNODE_SYNC_H
@@ -7,11 +7,15 @@
 #include "chain.h"
 #include "net.h"
 
+#include <univalue.h>
+
 class CMasternodeSync;
 
 static const int MASTERNODE_SYNC_FAILED          = -1;
 static const int MASTERNODE_SYNC_INITIAL         = 0; // sync just started, was reset recently or still in IDB
 static const int MASTERNODE_SYNC_WAITING         = 1; // waiting after initial to see if we can get more headers/blocks
+static const int MASTERNODE_SYNC_LIST            = 2;
+static const int MASTERNODE_SYNC_MNW             = 3;
 static const int MASTERNODE_SYNC_GOVERNANCE      = 4;
 static const int MASTERNODE_SYNC_GOVOBJ          = 10;
 static const int MASTERNODE_SYNC_GOVOBJ_VOTE     = 11;
@@ -19,6 +23,8 @@ static const int MASTERNODE_SYNC_FINISHED        = 999;
 
 static const int MASTERNODE_SYNC_TICK_SECONDS    = 6;
 static const int MASTERNODE_SYNC_TIMEOUT_SECONDS = 30; // our blocks are 2.5 minutes so 30 seconds should be fine
+
+static const int MASTERNODE_SYNC_ENOUGH_PEERS    = 6;
 
 extern CMasternodeSync masternodeSync;
 
@@ -30,9 +36,9 @@ class CMasternodeSync
 {
 private:
     // Keep track of current asset
-    int nCurrentAsset;
+    int nRequestedMasternodeAssets;
     // Count peers we've requested the asset from
-    int nTriedPeerCount;
+    int nRequestedMasternodeAttempt;
 
     // Time when current masternode asset sync started
     int64_t nTimeAssetSyncStarted;
@@ -49,12 +55,14 @@ public:
 
     void SendGovernanceSyncRequest(CNode* pnode, CConnman& connman);
 
-    bool IsFailed() { return nCurrentAsset == MASTERNODE_SYNC_FAILED; }
-    bool IsBlockchainSynced() { return nCurrentAsset > MASTERNODE_SYNC_WAITING; }
-    bool IsSynced() { return nCurrentAsset == MASTERNODE_SYNC_FINISHED; }
+    bool IsFailed() { return nRequestedMasternodeAssets == MASTERNODE_SYNC_FAILED; }
+    bool IsBlockchainSynced() { return nRequestedMasternodeAssets > MASTERNODE_SYNC_WAITING; }
+    bool IsMasternodeListSynced() { return nRequestedMasternodeAssets > MASTERNODE_SYNC_LIST; }
+    bool IsWinnersListSynced() { return nRequestedMasternodeAssets > MASTERNODE_SYNC_MNW; }
+    bool IsSynced() { return nRequestedMasternodeAssets == MASTERNODE_SYNC_FINISHED; }
 
-    int GetAssetID() { return nCurrentAsset; }
-    int GetAttempt() { return nTriedPeerCount; }
+    int GetAssetID() { return nRequestedMasternodeAssets; }
+    int GetAttempt() { return nRequestedMasternodeAttempt; }
     void BumpAssetLastTime(const std::string& strFuncName);
     int64_t GetAssetStartTime() { return nTimeAssetSyncStarted; }
     std::string GetAssetName();
@@ -69,8 +77,6 @@ public:
     void AcceptedBlockHeader(const CBlockIndex *pindexNew);
     void NotifyHeaderTip(const CBlockIndex *pindexNew, bool fInitialDownload, CConnman& connman);
     void UpdatedBlockTip(const CBlockIndex *pindexNew, bool fInitialDownload, CConnman& connman);
-
-    void DoMaintenance(CConnman &connman);
 };
 
 #endif
